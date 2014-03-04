944a85f1b966f2a1ff234b534e81e74d682b80df
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-03-04 17:51:31 -0500

    Kind of got the reviews scrolling

--- app/assets/javascripts/staticpages/fader.js
+++ app/assets/javascripts/staticpages/fader.js
@@ -1,33 +1,67 @@
-$(document).ready(function(){
-	console.log('fader loaded')
+console.log('fader loaded');	
 
-var ourQuotes = function(){
-
-	var quotes = $('#fader');
-	var quoteIndex = -1;
-
-	function showNextQuote(){
-		++quoteIndex;
-		quotes.eq(quoteIndex % quotes.length)
-			.fadeIn(1000)
-			.delay(3000)
-			.fadeOut(1000, showNextQuote);
-	}
-
-	showNextQuote();
-
-}();
+// var counter = 0;
+// var i = setInterval(function(){             
+//     $('li.faderName').first().fadeOut('faderName').fadeIn('showName')
+// 	$('li.faderReview').first().fadeOut('faderReview').fadeIn('showReview');
+// 	counter++;
+//     if(counter === 100) {
+//         clearInterval(i);
+//     }
+// }, 5000);
 
 
 
-	
-	
-	// $('#fader').find(function(){
-	// 	$('tr').hide
-	// setInterval(function(){
-	// 	$('tr').delay(1000).fadeIn(3000, function(){
+window.setInterval(function(){ 
+	$('li.faderName').first().removeClass('faderName').addClass('hideMe')
+    $('li.faderReview').first().removeClass('faderReview').addClass('hideMe')
+}, 1000);
 
-	// 	});
-	// }, 3000);
-	// });
-});
+
+//kind of works!
+// window.setInterval( function() {
+// 	$('li.faderName').first().removeClass('faderName').addClass('showName')
+// 	$('li.faderReview').first().removeClass('faderReview').addClass('showReview')
+// }, 500)
+
+// window.setInterval( function() {
+// 	$('li.showName').first().removeClass('showName').addClass('helloName')
+// 	$('li.showReview').first().removeClass('showReview').addClass('helloReview')
+// }, 2000)
+
+// window.setInterval( function() {
+// 	$('li.helloName').first().removeClass('helloName').addClass('hideMe')
+// 	$('li.helloReview').first().removeClass('helloReview').addClass('hideMe')
+// }, 500)
+
+
+
+
+//this works for the entire list
+// window.setInterval( function() {
+//   $('li.fader').each(function(index){$(this).fadeOut('li.fader')});
+//   $('li.fader').each(function(index){$(this).fadeIn('li.fader')});
+// }, 5000)
+
+
+//fading part on its own with fader
+// window.setInterval( function() {
+//   $('.fader').fadeOut('fader')
+//   $('.fader').fadeIn('fader')
+// }, 5000)
+
+//this does it once and stops
+//setInterval(function(){$('.fader').removeClass('fader')},3000);
+
+// function updateLoop() {
+//    $('.fader').removeClass('fader')
+// }
+
+// setInterval(updateLoop,500);
+
+//thisworks to remove and add a class.  kind of there.
+// $(document).ready(function(){
+// 	console.log('fader loaded')
+
+// 	$('.fader').removeClass('fader').addClass('hello');
+// });
