ff68e071a4067d1b8acb2dfa44e09591a2a513a6
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-03-04 10:37:56 -0500

    started fader

--- app/assets/javascripts/staticpages/fader.js
+++ app/assets/javascripts/staticpages/fader.js
@@ -1,4 +1,33 @@
 $(document).ready(function(){
 	console.log('fader loaded')
+
+var ourQuotes = function(){
+
+	var quotes = $('#fader');
+	var quoteIndex = -1;
+
+	function showNextQuote(){
+		++quoteIndex;
+		quotes.eq(quoteIndex % quotes.length)
+			.fadeIn(1000)
+			.delay(3000)
+			.fadeOut(1000, showNextQuote);
+	}
+
+	showNextQuote();
+
+}();
+
+
+
 	
+	
+	// $('#fader').find(function(){
+	// 	$('tr').hide
+	// setInterval(function(){
+	// 	$('tr').delay(1000).fadeIn(3000, function(){
+
+	// 	});
+	// }, 3000);
+	// });
 });
