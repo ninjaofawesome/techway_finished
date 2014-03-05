90decf24a49b21329c031cb9b854f83d30387b32
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-03-05 16:13:25 -0500

    made final sweep, looks good

--- app/assets/javascripts/staticpages/carousel.js
+++ app/assets/javascripts/staticpages/carousel.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
 
 	$('.carousel').carousel({
-		interval: 7000
+		interval: 3000
 	});
 
 });
---
+++ app/assets/javascripts/staticpages/visibility.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	console.log('visibility loaded')
+
+
+
+	// if user show navbar icons
+	// if admin hide navbar icons
+	//$('#visibility').hide();
+});
